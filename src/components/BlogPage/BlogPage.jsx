085eceb05cfb44fd6e098f65ebdff711d39e5f54
Sakib Hossain <sakibhossainceo.com>
2023-11-10 07:22:55 +0600

    side ver added

--- src/components/BlogPage/BlogPage.jsx
+++ src/components/BlogPage/BlogPage.jsx
@@ -2,13 +2,16 @@
 import React from 'react';
 import './Blogpage.css'
 import Blog from '../Blog/Blog';
+import BookMark from '../BookMark/BookMark';
+
 
 const BlogPage = () => {
     return (
         <div className='blog-page'>
-            {
+            
                 <Blog></Blog>
-            }
+                
+            <BookMark></BookMark>
         </div>
     );
 };
---
+++ src/components/BookMark/BookMark.jsx
@@ -0,0 +1,33 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import './BookMark.css'
+
+const BookMark = () => {
+    return (
+        <div className='container'>
+            <div className="spent-time">
+                <h3>Spent time on read : 177 min</h3>
+            </div>
+            <div className="bookMark-container">
+                <h3>Bookmarked Blogs : 8</h3>
+                <div className="bookMark">
+                    <h5>Master Microsoft Power Platform and Become an In-Demand!</h5>
+                </div>
+                <div className="bookMark">
+                    <h5>Master Microsoft Power Platform and Become an In-Demand!</h5>
+                </div>
+                <div className="bookMark">
+                    <h5>Master Microsoft Power Platform and Become an In-Demand!</h5>
+                </div>
+                <div className="bookMark">
+                    <h5>Master Microsoft Power Platform and Become an In-Demand!</h5>
+                </div>
+                <div className="bookMark">
+                    <h5>Master Microsoft Power Platform and Become an In-Demand!</h5>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default BookMark;
